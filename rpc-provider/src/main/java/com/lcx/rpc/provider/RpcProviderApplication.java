8b7b886d6cd2f08f6b282d9f2ee72e01b765feca
L1Chenxv <943429931@qq.com>
2024-08-30 00:09:19 +0800

    opt: delete unused imports

--- rpc-provider/src/main/java/com/lcx/rpc/provider/RpcProviderApplication.java
+++ rpc-provider/src/main/java/com/lcx/rpc/provider/RpcProviderApplication.java
@@ -1,15 +1,8 @@
 package com.lcx.rpc.provider;
 
-import com.lcx.rpc.protocol.MsgType;
-import com.lcx.rpc.serialization.SerializationFactory;
-import com.lcx.rpc.serialization.SerializationTypeEnum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
 
 /**
  * @description:
